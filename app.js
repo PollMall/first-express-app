bef43e131aaee96b8618381d9d3984baecac7d9e
Paul Popa <16paul.popa@gmail.com>
2022-01-16 15:55:49 +0200

    add basic routing

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require("express");
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Welcome to the home page!");
 });
 
-const port = process.env.PORT || 3000;
+app.get("/posts", (req, res) => {
+  res.send("This is /posts path");
+});
+
+app.get("/posts/:id", (req, res) => {
+  res.send(`This is /posts/${req.params.id} path`);
+});
 
 app.listen(3000, () => {
   console.log(`Server started on port ${port} 🚀`);
