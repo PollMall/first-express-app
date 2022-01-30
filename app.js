5b148c778ec4ed9fcb1caa901f98bf8ecc52d86e
Paul Popa <paul.popa@moduscreate.com>
2022-01-30 19:03:46 +0200

    play with basic middleware functons

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require("express");
 
 const app = express();
 
+// Middlewares
+app.use(logger);
+
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("get on /");
+});
+
+app.get("/test", (req, res) => {
+  res.send("get on /test");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Not found, sorry!");
 });
 
 const port = process.env.PORT || 3000;
@@ -11,3 +22,11 @@ const port = process.env.PORT || 3000;
 app.listen(3000, () => {
   console.log(`Server started on port ${port} 🚀`);
 });
+
+function logger(req, res, next) {
+  console.log("======== START ========");
+  console.log("logger");
+  console.log(`path: ${req.path}`);
+  next();
+  console.log("========= END =========");
+}
