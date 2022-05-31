3908eabcbd033ff1335399d2022f51b8ea400743
Paul Popa <paul.popa@moduscreate.com>
2022-05-31 20:04:47 +0300

    fix router

--- routes/index.js
+++ routes/index.js
@@ -17,25 +17,25 @@ router
   .get((req, res) => {
     res.render("add");
   })
-  .post(async (req, res) => {
-    console.log(req.body);
+  .post([
+    validateBody,
+    async (req, res) => {
+      console.log(req.body);
 
-    if (req.body && req.body.title) {
       const newTodo = {
         title: req.body.title,
         description: req.body.description,
       };
+
       try {
-        await saveTodo(newTodo);
-        res.send({ data: newTodo });
+        const savedTodo = await saveTodo(newTodo);
+        res.send({ data: savedTodo });
       } catch (err) {
         console.error(err);
         res.status(500).send({ error: "Could not save the todo." });
       }
-    } else {
-      res.status(400).send({ error: "Please provide at least a title for todo" });
-    }
-  });
+    },
+  ]);
 
 router
   .route("/edit/:id")
@@ -52,45 +52,41 @@ router
     const removedTodo = await removeTodo(req.params.id);
 
     if (removedTodo) {
-      res.send({ data: removeTodo });
+      res.send({ data: removedTodo });
     } else {
       res.status(404).send({ error: "Todo not found!", data: {} });
     }
   })
-  .put(async (req, res) => {
-    console.log(req.body);
+  .put([
+    validateBody,
+    async (req, res) => {
+      console.log(req.body);
 
-    if (req.body && req.body.title) {
       const newTodo = {
         id: req.params.id,
         title: req.body.title,
         description: req.body.description,
       };
+
       try {
-        await updateTodo(newTodo);
-        res.send({ data: newTodo });
+        const updatedTodo = await updateTodo(newTodo);
+        res.send({ data: updatedTodo });
       } catch (err) {
         console.error(err);
         res.status(500).send({ error: "Could not update the todo." });
       }
-    } else {
-      res.status(400).send({ error: "Please provide at least a title for todo" });
-    }
-  });
-
-router.get("/test", async (req, res) => {
-  res.send(await db.getAll());
-});
+    },
+  ]);
 
 router.get("*", (req, res) => {
   res.status(404).render("404");
 });
 
-function validate(req, res) {
+function validateBody(req, res, next) {
   if (req.body && req.body.title) {
     return next();
   }
-  res.status(400).send("Please provide a title for todo");
+  res.status(400).send({ error: "Please provide at least a title for todo" });
 }
 
 module.exports = router;
